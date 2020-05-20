428a408b703ae2705bf33a473cebb3502c7a70a5
TheycalledmeMrGlass <shojaeifarzad79@yahoo.com>
2020-05-20 13:27:14 +0430

    HW6

---
+++ HW6/HW6.1/src/com/company/User.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class User {
+  public int idcode;
+  public String username;
+  public String email;
+  public int password;
+  public int age;
+  public String education;
+
+
+
+
+}
